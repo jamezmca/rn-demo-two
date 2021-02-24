3d3ceaf371eba1d91c7dcfa68bd04d24e90852c9
James McArthur <jamesmca@live.com>
2021-02-23 20:53:49 -0800

    Added in default styles stylesheet
    used to pull out default styles across application

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,7 +4,8 @@ import Card from '../components/Card';
 import Colors from '../constants/colors';
 import Input from '../components/Input';
 import NumberContainer from '../components/NumberContainer';
-
+import BodyText from '../components/BodyText';
+import TitleText from '../components/TitleText';
 
 const StartGameScreen = props => {
     const [enteredValue, setEnteredValue] = useState('');
@@ -37,7 +38,7 @@ const StartGameScreen = props => {
     if (confirmed) {
         confirmedOutput =
             <Card style={styles.summaryContainer}>
-                <Text>You selected:</Text>
+                <BodyText>You selected:</BodyText>
                 <NumberContainer>{selectedNumber}</NumberContainer>
                 <Button title="START GAME" onPress={() => props.onStartGame(selectedNumber)}/>
             </Card>
@@ -48,9 +49,9 @@ const StartGameScreen = props => {
             Keyboard.dismiss();
         }}>
             <View style={styles.screen}>
-                <Text style={styles.title}>Start a New Game!</Text>
+                <TitleText style={styles.title}>Start a New Game!</TitleText>
                 <Card style={styles.inputContainer}>
-                    <Text>Select a Number</Text>
+                    <BodyText>Select a Number</BodyText>
                     <Input style={styles.input} blurOnSubmit autoCapitalize='none' autoCorrect={false} keyboardType="number-pad" maxLength={2} onChangeText={numberInputHandler} value={enteredValue} />
                     <View style={styles.buttonContainer}>
                         <View style={styles.button}>
@@ -76,7 +77,8 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 20,
         marginBottom: 10,
-        marginTop: 10
+        marginTop: 10,
+        fontFamily: 'open-sans-bold'
     },
     inputContainer: {
         width: 300,
@@ -103,6 +105,9 @@ const styles = StyleSheet.create({
         alignItems: 'center',
     
     },
+    text: {
+        fontFamily: 'open-sans'
+    }
 
 
 });
